65cd6e6e5c781f13000e64262bf203a78dcc8075
starjjbang <starjjbang@gmail.com>
2023-08-29 20:12:00 +0900

    3st com

--- 1_12_22_single_cell.R
+++ 1_12_22_single_cell.R
@@ -2,3 +2,7 @@ a=c(1,2,3,4,5)
 #https://www.youtube.com/watch?v=1I3hMwQU6GU&t=2454s
 #starjjbang@7920T:~/code_git$ git branch -M main
 #starjjbang@7920T:~/code_git$ git push -u origin main
+
+#git add . : 변경사항 전체 넣기
+#git commit -m "3단어이내추천"
+#git push origin main
