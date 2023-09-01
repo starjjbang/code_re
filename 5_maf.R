95700bb0a98abc38d34c7d03f2f38ae3591843f7
starjjbang <starjjbang@gmail.com>
2023-09-01 17:01:10 +0900

    GWAS_proj_3st

--- 5_maf.R
+++ 5_maf.R
@@ -1,23 +1,9 @@
-source('/home/starjjbang/an_proj/4_GWAS/code/GWAS_function.R')
-in_lab = "G1"
-vcf_dir = '/home/starjjbang/an_proj/4_GWAS/result/5_freq/vcf/'
-re_dir = '/home/starjjbang/an_proj/4_GWAS/result/7_MAF_test/'
-bcftools_dir = '/home/starjjbang/an_proj/4_GWAS/bcftools/bcftools'
-p_dir = paste0(re_dir,'1_merge_VCF/')
-  
-f_lst = list.files(vcf_dir,pattern = in_lab)
-lab_1 = paste(f_lst[grep("*.vcf.gz$",f_lst)],collapse = " ")
-q3 = paste0('vcf-merge ',vcf_dir,lab_1,' > ',
-            re_dir,'1_merge_VCF/',in_lab,'.vcf') 
-print(q3)
-q = paste0(bcftools_dir," view --max-alleles 2 --exclude-types indels ",
-           paste0(p_dir,in_lab,'.vcf'), ' > ', p_dir,'1_filter_',in_lab,'.vcf')
-system(q)
-runplink(paste0(" --vcf ",p_dir,'1_filter_',in_lab,'.vcf',
-                " --make-bed --out "), paste0(p_dir,'2_filter_',in_lab))
-runplink(paste0(" --bfile ",p_dir,'2_filter_',in_lab," --freq --out "),
-         paste0(p_dir,'3_filter_',in_lab,'.vcf'))
+source('/home/starjjbang/code_git/GWAS_function.R')
 #https://blog.naver.com/sw4r/221541700667
 #MAF = 0.5 : 4 명 8 allele 중 T가 4명이 다 있었음
 #A1에 대한 MAF https://mopipe.tistory.com/13
+MAF_cal("G1")
+MAF_cal("G2")
+MAF_cal("G3")
+MAF_cal("G4")
 
--- GWAS_function.R
+++ GWAS_function.R
@@ -384,6 +384,26 @@ m <- function(x, chr = "CHR", bp = "BP", p = "P", snp = "SNP", col = c("gray10",
   par(xpd = FALSE)
 }
 
+MAF_cal <- function(in_lab){
+  vcf_dir = '/home/starjjbang/an_proj/4_GWAS/result/5_freq/vcf/'
+  re_dir = '/home/starjjbang/an_proj/4_GWAS/result/7_MAF_test/'
+  bcftools_dir = '/home/starjjbang/an_proj/4_GWAS/bcftools/bcftools'
+  p_dir = paste0(re_dir,'1_merge_VCF/')
+  f_lst = list.files(vcf_dir,pattern = in_lab)
+  lab_1 = paste(sapply(f_lst[grep("*.vcf.gz$",f_lst)], function(x) paste0(vcf_dir,x)),
+                collapse = " ")
+  q3 = paste0('vcf-merge ',lab_1,' > ',
+              re_dir,'1_merge_VCF/',in_lab,'.vcf') 
+  system(q3)
+  q = paste0(bcftools_dir," view --max-alleles 2 --exclude-types indels ",
+             paste0(p_dir,in_lab,'.vcf'), ' > ', p_dir,'1_filter_',in_lab,'.vcf')
+  system(q)
+  runplink(paste0(" --vcf ",p_dir,'1_filter_',in_lab,'.vcf',
+                  " --make-bed --out "), paste0(p_dir,'2_filter_',in_lab))
+  runplink(paste0(" --bfile ",p_dir,'2_filter_',in_lab," --freq --out "),
+           paste0(p_dir,'3_filter_',in_lab,'.vcf'))
+}
+
 
 
 # library(qqman)
